b10ef9c960abc563b5db84b4de3a9c53b3ba8386
cmotek <cmotek1122@gmail.com>
2023-04-14 16:35:04 -0500

    second commit

--- 22_unpacking_keyword_arguments/code.py
+++ 22_unpacking_keyword_arguments/code.py
@@ -6,8 +6,26 @@
 
 #named(**details)
 
-def named(**kwargs):
+#def named(**kwargs):
+  #  print(kwargs)
+
+#def print_nicely(**kwargs):
+#    named(**kwargs)
+#    for arg, value in kwargs.items():
+#        print(f"{arg}: {value}")
+
+#print_nicely(name="Bob", age=25)
+
+
+
+#def both(*args, **kwargs):
+#    print(args)
+#    print(kwargs)
+
+#both(1, 3, 5, name="Bob")
+
+def myfunction(**kwargs):
     print(kwargs)
 
-def print_nicely(**kwargs):
-    named(**kwargs)
+myfunction(**"Bob")
+myfunction(**None)
---
+++ 25_classmethod_staticmethod/code.py
@@ -0,0 +1,5 @@
+class ClassTest:
+    def instance_method(self):
+        print(f"Called instance_method of {self}")
+
+        
