a570ee4acbc4ab1ac0f763199238e319747a91fa
cmotek <cmotek1122@gmail.com>
2023-04-21 15:35:25 -0500

    fifthcommit

--- rest-api-project/app.py
+++ rest-api-project/app.py
@@ -1,48 +1,33 @@
-from flask import Flask, request
-from db import items, stores
-import uuid
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from flask_smorest import Api
 
+from db import db
+import models
 
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
-
-@app.post("/item")
-def create_item(name):
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["PROPOGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
     
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-    return item, 201
+    api = Api(app)
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
- 
+    with app.app_context():
+        db.create_all()
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        return {"message": "Store not found"}, 404
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        return {"message": "Item not found"}, 404
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    
+    return app
