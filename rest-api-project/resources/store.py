82a06b30e852bd75d06468b867f3f0c253ca3a91
cmotek <cmotek1122@gmail.com>
2023-04-26 16:37:45 -0500

    seventhcommit

--- rest-api-project/resources/store.py
+++ rest-api-project/resources/store.py
@@ -9,7 +9,7 @@ from schemas import StoreSchema
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
-@blp.route("/store/<string:store_id>")
+@blp.route("/store/<int:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
@@ -29,7 +29,7 @@ class StoreList(MethodView):
         return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
-    @blp.response(200, StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(self, store_data):
         store = StoreModel(**store_data)
         try:
--- rest-api-project/resources/tag.py
+++ rest-api-project/resources/tag.py
@@ -9,7 +9,7 @@ from schemas import TagSchema, TagAndItemSchema
 blp = Blueprint("Tags", "tags", description="Operations on tags")
 
 
-@blp.route("/store/<string:store_id>/tag")
+@blp.route("/store/<int:store_id>/tag")
 class TagsInStore(MethodView):
     @blp.response(200, TagSchema(many=True))
     def get(self, store_id):
@@ -37,7 +37,7 @@ class TagsInStore(MethodView):
         return tag
     
 
-@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+@blp.route("/item/<int:item_id>/tag/<int:tag_id>")
 class LinkTagsToItem(MethodView):
     @blp.response(201, TagSchema)
     def post(self, item_id, tag_id):
@@ -69,7 +69,7 @@ class LinkTagsToItem(MethodView):
 
         return {"message": "Item removed from tag", "item": item, "tag": tag}
         
-@blp.route("/tag/<string:tag_id>")
+@blp.route("/tag/<int:tag_id>")
 class Tag(MethodView):
     @blp.response(200, TagSchema)
     def get(self, tag_id):
@@ -82,7 +82,7 @@ class Tag(MethodView):
         example={"message": "Tag deleted."}
     )
     @blp.alt_response(404, description="Tag not found.")
-    @blp.alt_response(400, description="Returned if the tag is assigned ot one or more items. In this case, the tag is not deleted.")
+    @blp.alt_response(400, description="Returned if the tag is assigned to one or more items. In this case, the tag is not deleted.")
     def delete(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
 
@@ -90,5 +90,9 @@ class Tag(MethodView):
             db.session.delete(tag)
             db.session.commit()
             return {"message": "Tag deleted."}
-        
+        abort(
+            400,
+            message="Could not delete tag. Make sure tag is not associated with any items, then try again.",  # noqa: E501
+        )
+
         
